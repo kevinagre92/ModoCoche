63c838d7e31c376440a55c26790b8ccff2506046
Kevin Muñoz <tgdkevinmunoz@gmail.com>
2026-01-13 14:05:51 +0000

    Initial Commit

---
+++ ModoCoche/ModoCocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModoCocheApp.swift
+//  ModoCoche
+//
+//  Created by Kevin Muñoz G on 13/1/26.
+//
+
+import SwiftUI
+
+@main
+struct ModoCocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
